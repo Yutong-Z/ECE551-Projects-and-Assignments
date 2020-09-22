4a56da803e20cdc6a7ddfc44fb8448552547a003
yz566 <yz566@duke.edu>
2020-09-21 22:15:41 -0400

    finished 052

--- 052_fix_vg_encr/encrypt.c
+++ 052_fix_vg_encr/encrypt.c
@@ -1,37 +1,42 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
-void encrypt(FILE * f, int key, FILE * outfile){
-  char * line;
-  size_t sz;
-  while (getline(&line,&sz, f) >= 0) {
+void encrypt(FILE * f, int key, FILE * outfile) {
+  char * line = NULL;
+  size_t sz = 0;
+  ssize_t len = 0;
+  while ((len = getline(&line, &sz, f)) >= 0) {
     char * ptr = line;
-    while (*ptr != '\0') {
-      int c = *ptr;
+    int i = 0;
+    while (ptr[i] != '\0') {
+      int c = ptr[i];
       if (isalpha(c)) {
-	c = tolower(c);
-	c -= 'a';
-	c += key;
-	c %= 26;
-	c += 'a';
+        c = tolower(c);
+        c -= 'a';
+        c += key;
+        c %= 26;
+        c += 'a';
       }
-      *ptr = c;
-      ptr++;
+      ptr[i] = c;
+      i++;
     }
     fprintf(outfile, "%s", line);
+    line = NULL;
+    free(ptr);
   }
+  free(line);
 }
 
 int main(int argc, char ** argv) {
   if (argc != 3) {
-    fprintf(stderr,"Usage: encrypt key inputFileName\n");
+    fprintf(stderr, "Usage: encrypt key inputFileName\n");
     return EXIT_FAILURE;
   }
   int key = atoi(argv[1]);
   if (key == 0) {
-    fprintf(stderr,"Invalid key (%s): must be a non-zero integer\n", argv[1]);
+    fprintf(stderr, "Invalid key (%s): must be a non-zero integer\n", argv[1]);
     return EXIT_FAILURE;
   }
   FILE * f = fopen(argv[2], "r");
@@ -40,11 +45,13 @@ int main(int argc, char ** argv) {
     return EXIT_FAILURE;
   }
   //outfileNAme is argv[2] + ".txt", so add 4 to its length.
-  char * outFileName = malloc((strlen(argv[2]) + 4) * sizeof(*outFileName));
+  // +1 for '\0' +4 for ".enc"
+  char * outFileName = malloc((strlen(argv[2]) + 9) * sizeof(*outFileName));
   strcpy(outFileName, argv[2]);
   strcat(outFileName, ".enc");
   FILE * outFile = fopen(outFileName, "w");
-  encrypt(f,key, outFile);
+  encrypt(f, key, outFile);
+  free(outFileName);
   if (fclose(outFile) != 0) {
     perror("Failed to close the input file!");
     return EXIT_FAILURE;
