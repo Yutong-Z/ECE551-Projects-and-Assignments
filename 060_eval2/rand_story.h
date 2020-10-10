4eaffbf12283b966051330368a8a23bcc292a3d7
yz566 <yz566@duke.edu>
2020-10-09 20:10:49 -0400

    finished step1

--- 060_eval2/rand_story.h
+++ 060_eval2/rand_story.h
@@ -1,8 +1,12 @@
 #ifndef __RAND_STORY_H__
 #define __RAND_STORY_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "provided.h"
 
-//any functions you want your main to use
+void printStoryLine(char * line);
+void parseTemplate(FILE * f);
 
 #endif
--- 060_eval2/story-step1.c
+++ 060_eval2/story-step1.c
@@ -1,57 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#include "provided.h"
 #include "rand_story.h"
-
-void printStoryLine(char * line) {
-  size_t i = 0;
-  int j = 0;
-  size_t catLen = 0;
-  char * cat = NULL;
-  while (line[i] != '\0') {
-    if (line[i] != '_' && j == 0) {  // out of blank
-      printf("%c", line[i]);
-      i++;
-    }
-    if (line[i] == '_' && j == 0) {  // new blank area
-      i++;
-      j = 1;
-    }
-    if (line[i] != '_' && j == 1) {  //in blank
-      cat = realloc(cat, (catLen + 1) * sizeof(*cat));
-      cat[catLen] = line[i];
-      catLen++;
-      i++;
-    }
-    if (line[i] == '_' && j == 1) {  //end of blank
-      cat = realloc(cat, (catLen + 1) * sizeof(*cat));
-      cat[catLen] = '\0';
-      const char * word = chooseWord(cat, NULL);  // step1, get a pointer to "cat"
-      catLen = 0;
-      j = 0;
-      i++;
-      // print word into story
-      printf("%s", word);
-    }
-  }
-  free(cat);
-  if (j == 1) {  //no matching '_' with a pervious '_'
-    fprintf(stderr, "Underscore not matcing in line %s", line);
-    exit(EXIT_FAILURE);
-  }
-}
-
-void parseTemplate(FILE * f) {
-  char * line = NULL;
-  size_t linecap;
-  // parsing temlapte file, print lines with blank converted
-  while (getline(&line, &linecap, f) >= 0) {
-    printStoryLine(line);
-  }
-  free(line);
-}
 
 int main(int argc, char ** argv) {
   if (argc < 2) {
