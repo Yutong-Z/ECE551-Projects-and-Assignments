ff86147c041e58839af1411407324e2b67e8936d
yz566 <yz566@duke.edu>
2020-10-14 21:38:25 -0400

    corrected 079

--- 079_sort_cpp/sortLines.cpp
+++ 079_sort_cpp/sortLines.cpp
@@ -5,29 +5,39 @@
 #include <vector>
 
 int main(int argc, char ** argv) {
-  std::string line;
-  std::vector<std::string> lines;
   if (argc == 1) {
+    std::string line;
+    std::vector<std::string> lines;
     while (getline(std::cin, line)) {
       lines.push_back(line);
     }
+    std::sort(lines.begin(), lines.end());
+    std::vector<std::string>::iterator it = lines.begin();
+    while (it != lines.end()) {
+      std::cout << *it << "\n";
+      ++it;
+    }
   }
-  else {
+  else if (argc > 1) {
     for (int i = 1; i < argc; i++) {
+      std::string line;
+      std::vector<std::string> lines;
       std::ifstream f(argv[i]);
       if (!f.is_open()) {
         std::cerr << "Can not open file " << argv[i] << std::endl;
+        exit(EXIT_FAILURE);
       }
       while (getline(f, line)) {
         lines.push_back(line);
       }
+      std::sort(lines.begin(), lines.end());
+      std::vector<std::string>::iterator it = lines.begin();
+      while (it != lines.end()) {
+        std::cout << *it << "\n";
+        ++it;
+      }
       f.close();
     }
   }
-  std::sort(lines.begin(), lines.end());
-  std::vector<std::string>::iterator it = lines.begin();
-  while (it != lines.end()) {
-    std::cout << *it << "\n";
-    ++it;
-  }
+  return EXIT_SUCCESS;
 }
